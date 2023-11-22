c58923073597cfd4cdaaade80046cfc7afcc87b1
Rai Siqueira <rai93siqueira@gmail.com>
2023-11-22 18:18:59 -0300

    v0.0.1
    Signed-off-by: Rai Siqueira <rai93siqueira@gmail.com>

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'EruptionJS',
+			social: {
+				github: 'https://github.com/eruptionjs',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: [
+				"./src/styles/base.css",
+			]
+		}),
+	],
+});
---
+++ src/content/docs/guides/new-project.md
@@ -0,0 +1,11 @@
+---
+title: New Project
+description: Starting a new project with EruptionJS.
+---
+
+Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
+Writing a good guide requires thinking about what your users are trying to do.
+
+## Further reading
+
+- WIP
